258f6de34e7e0dd3fbdd845c55fdfc40581df68c
Dinendra Dolai <dinendra.dolai@gmail.com>
2023-09-15 05:09:23 +0530

    Added feature A

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! this is React
+          Hello! this is React. Added a new feature
         </p>
         <a
           className="App-link"
